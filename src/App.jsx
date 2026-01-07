acc7459aade3af035d3c2e415f3b894b5b63f7a9
Shodunke Haleema <shodunkea4@gmail.com>
2026-01-07 15:17:18 +0100

    Fix ScrollToTop import path for Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./components/Home";
 import Services from "./components/Services";
 import Aboutus from "./components/Aboutus";
 import Contact from "./components/Contact";
-import ScrollToTop from "./Components/ScrollToTop";
+import ScrollToTop from './components/ScrollToTop';
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
